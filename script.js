ca49c093a61e4be1f4aab9c6c0c3aa45a79950cf
yalbal4 <104597066+yalbal4@users.noreply.github.com>
2023-01-13 21:23:46 -0500

    add playRound function
    plays one round of rock paper scissors, comparing player choice
    vs computer choice

--- script.js
+++ script.js
@@ -6,5 +6,24 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    
+    playerSelection = playerSelection.toLowerCase()
+    console.log("Player: " + playerSelection + ". Computer: " + computerSelection)
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'Rock') {return "Tie. Rock ties Rock."}
+        if (computerSelection === 'Paper') {return "You lose! Rock loses to Paper."}
+        if (computerSelection === 'Scissors') {return "You win! Rock beats Scissors."}
+    }
+
+    if (playerSelection === 'paper') {
+        if (computerSelection === 'Rock') {return "You win! Paper beats Rock."}
+        if (computerSelection === 'Paper') {return "Tie. Paper ties Paper."}
+        if (computerSelection === 'Scissors') {return "You lose! Paper loses to Scissors"}
+    }
+
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'Rock') {return "You lose! Scissors loses to Rock."}
+        if (computerSelection === 'Paper') {return "You win! Scissors beats Paper."}
+        if (computerSelection === 'Scissors') {return "Tie. Scissors ties Scissors"}
+    }
 }
